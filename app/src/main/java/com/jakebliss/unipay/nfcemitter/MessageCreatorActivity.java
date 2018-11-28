e7ec1e4e058ee7c663ad78dc157947ddf37198d6
Zakary Bliss <zakarybliss@ucsb.edu>
2018-11-27 20:32:36 -0800

    Initial commit

---
+++ app/src/main/java/com/jakebliss/unipay/nfcemitter/MessageCreatorActivity.java
@@ -0,0 +1,13 @@
+package com.jakebliss.unipay.nfcemitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageCreatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_creator);
+    }
+}
