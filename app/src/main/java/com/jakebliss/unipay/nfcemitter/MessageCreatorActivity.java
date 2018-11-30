74e962de555740d8c8c822aad935ff8a7f38c150
Zakary Bliss <zakarybliss@ucsb.edu>
2018-11-29 19:20:12 -0800

    Created main activity, checked for permissions, added functionality to link with NFC devices.

--- app/src/main/java/com/jakebliss/unipay/nfcemitter/MessageCreatorActivity.java
+++ app/src/main/java/com/jakebliss/unipay/nfcemitter/MessageCreatorActivity.java
@@ -1,13 +1,63 @@
+// Followed tutorial from: https://www.survivingwithandroid.com/2016/01/nfc-tag-writer-android.html
 package com.jakebliss.unipay.nfcemitter;
 
+import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.nfc.NfcAdapter;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MessageCreatorActivity extends AppCompatActivity {
+    private NFCManager nfcMger = null;
+    final private int NFC_PERMISSION_CODE = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        nfcMger = new NFCManager(this);
         setContentView(R.layout.activity_message_creator);
+
+        if (ContextCompat.checkSelfPermission( MessageCreatorActivity.this, Manifest.permission.NFC)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MessageCreatorActivity.this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE}, NFC_PERMISSION_CODE);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            nfcMger.verifyNFC();
+            Intent nfcIntent = new Intent(this, getClass());
+            nfcIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, nfcIntent, 0);
+            IntentFilter[] intentFiltersArray = new IntentFilter[]{};
+            String[][] techList = new String[][]{
+                    {android.nfc.tech.Ndef.class.getName()},
+                    {android.nfc.tech.NdefFormatable.class.getName()}
+            };
+            NfcAdapter nfcAdpt = NfcAdapter.getDefaultAdapter(this);
+            nfcAdpt.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
+    {
+        switch (requestCode) {
+            case NFC_PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    }
+                break;
+        }
     }
 }
